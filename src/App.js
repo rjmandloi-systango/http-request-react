dca8f45eb48987e35c31605b263b33af79a2b538
rjmandloi-systango <rjmandloi@isystango.com>
2022-07-15 18:05:18 +0530

    Sending http request and getting movies

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useCallback, useEffect, useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      // console.log(response);
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+      const data = await response.json();
+      const transformedMovies = data.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, []);
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {isLoading ? <p>Loading...</p> : <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 ? (
+          <p>No data found</p>
+        ) : (
+          <MoviesList movies={movies} />
+        )}
+        {!isLoading && error && <p>{error}</p>}
+      </section>
+    </React.Fragment>
+  );
+}
+export default App;
